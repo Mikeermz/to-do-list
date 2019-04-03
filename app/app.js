15eacb4080f33bd71c65eb85cb039427a717c290
Mikee Rmz <mikee.rmz@gmail.com>
2019-04-03 00:50:18 -0600

    add path not found in app js
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- app/app.js
+++ app/app.js
@@ -18,6 +18,9 @@ app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
 app.use(bodyParser.json());
 app.use("/api/v1",router);
 app.use(cors());
+app.use((req, res, next)=> {
+  res.status(404).json({message: 'Endpoint Not Fount'})
+})
 
 // DB Conection
 
