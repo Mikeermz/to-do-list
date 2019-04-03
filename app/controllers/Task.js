c2e6ddaedf0b97b0a3aaf10b4d9eaf7ca521b362
Mikee Rmz <mikee.rmz@gmail.com>
2019-04-03 00:03:34 -0600

    Update task controller and add unique error to create new task
    Signed-off-by: Mikee Rmz <mikee.rmz@gmail.com>

--- app/controllers/Task.js
+++ app/controllers/Task.js
@@ -1,9 +1,12 @@
-const { createTask, getAllTask } = require("../actions");
+const { createTask, getAllTask, getTaskByName } = require("../actions");
 
 const newTask = (req, res) => {
 	createTask(req.body).then(
-		task => res.status(201).json({ "message": "Task has been creted"})
-	).catch(e => res.status(400).send(e));
+		(task) => res.status(201).json({ "message": "Task has been creted"})
+	).catch( (error) => {
+    if (error.code === 11000) return res.status(409).json({message: 'Duplicate name'})
+    return res.status(400).json(error)
+  });
 };
 
 const getTasks = async (req, res) => {
@@ -14,7 +17,16 @@ const getTasks = async (req, res) => {
     });
 };
 
+const updateTask = async (req, res) => {
+  getTaskByName(req.params.name)
+        .then((task) => {
+          if (!task) res.status(404).json({ "message": "Task does not exist" });
+		      res.status(200).json(task);
+        }).catch( error => res.status(400).json(error))
+}
+
 module.exports = { 
   newTask,
-  getTasks
+  getTasks,
+  updateTask
 };
